0629045a5b15e4bd54ab9642121e27f092149814
JhonAroni <jhonyaroni650@gmail.com>
2026-03-05 23:09:48 -0500

    correccion del apartado noticas movill

--- resources/views/noticias/show.blade.php
+++ resources/views/noticias/show.blade.php
@@ -30,7 +30,7 @@
 <div class="article-layout">
 
     {{-- CONTENIDO PRINCIPAL --}}
-    <article class="article-main" data-aos="fade-up">
+    <article class="article-main" style="order:0;" data-aos="fade-up">
 
         {{-- Imagen de portada --}}
         @if($noticia->imagen)
@@ -84,7 +84,7 @@
     </article>
 
     {{-- SIDEBAR --}}
-    <aside class="article-sidebar">
+    <aside class="article-sidebar" style="order:1;">
 
         {{-- Noticias relacionadas --}}
         @if($relacionadas->count() > 0)
